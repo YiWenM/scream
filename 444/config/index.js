5464f407836efdf05876b616291a5a4db53ea50f
Your Name <y18242016486@163.com>
2018-11-21 07:57:44 +0800

    333

--- 444/config/index.js
+++ 444/config/index.js
@@ -10,7 +10,14 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+        '/v2':{
+            arget:" https://m.wowdsgn.com",
+                host:"m.wowdsgn.com",
+                changeOrigin:true
+        }
+
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
